c8924998e493c25d4a57b7f9f1f04d97b7cb5fcb
Praveen <111781869+pscode22@users.noreply.github.com>
2024-01-27 16:19:05 +0530

    commit to main

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className=" flex-center min-h-screen w-full bg-primary-50 bg-dotted-pattern bg-cover bg-fixed bg-center">
+      {children}
+    </div>
+  );
+}
