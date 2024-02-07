3be3b08f650006ad3feca0490d9b90c58e1396f5
Praveen <111781869+pscode22@users.noreply.github.com>
2024-02-07 16:26:14 +0530

    changes to clerk-webhook

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -54,8 +54,6 @@ export async function POST(req: Request) {
   // const { id } = evt.data;
   const eventType = evt.type;
 
-  console.log('eventTYpe')
-
   // User creation webhook.
   if(eventType === 'user.created') {
     const { id, email_addresses, image_url, first_name, last_name, username } = evt.data;
@@ -70,9 +68,8 @@ export async function POST(req: Request) {
     }
 
     const newUser = await createUser(user);
-    console.log('newUser')
+    
     if(newUser) {         
-      console.log('await metaData') 
        await clerkClient.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id
